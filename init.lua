cc9d2741acac5cbd400bb3e255f14be3ffa472db
CrankyOctopus <octopus>
2026-05-17 10:31:36 -0700

    final fix for colors

--- init.lua
+++ init.lua
@@ -41,12 +41,3 @@ vim.opt.clipboard = "unnamedplus"
 -- Force TrueColor and prevent SSH detection lag
 vim.opt.termguicolors = true
 
--- This autocommand forces the colorscheme to re-apply 
--- once the terminal UI is fully attached over the network
-vim.api.nvim_create_autocmd("UIEnter", {
-    callback = function()
-        vim.o.background = "dark"
-        vim.cmd.colorscheme('github_light_default')
-    end
-})
-
