189950fb3a71332f120badfecbb9dedaa1f97ff5
CrankyOctopus <octopus>
2026-06-26 13:10:50 -0700

    fixed diffview buffer naming

--- lua/git.lua
+++ lua/git.lua
@@ -8,14 +8,7 @@ require("neogit").setup({
     diff_view = "side_by_side",
 })
 
-require("diffview").setup({
-    hooks = {
-        diff_buf_read = function(bufnr)
-            local name = "DiffView_" .. bufnr
-            pcall(vim.api.nvim_buf_set_name, bufnr, name)
-        end,
-    },
-})
+require("diffview").setup({})
 
 require('gitsigns').setup({
     diff_opts = {
